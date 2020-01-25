4a1a84fce322ed41044a40fd667ab9595df56dd9
Marissa Phul <marissa.ten@gmail.com>
2020-01-25 14:16:37 -0500

    reader

---
+++ reader.go
@@ -0,0 +1,18 @@
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct {}
+
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
+func (r MyReader) Read(b []byte) (n int, err error) {
+	b = b[:cap(b)]
+    for i := range b {
+        b[i] = 'A'
+    }
+    return cap(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
