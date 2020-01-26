9aa65daf44ea23aeaa4e9ec8b930291cfa5325fe
Marissa Phul <marissa.ten@gmail.com>
2020-01-25 19:49:48 -0500

    ebinary trees

---
+++ equivalent-binary-trees.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	// recursively walk the tree
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+}
+
+// helper walk method
+func Walker(t *tree.Tree, ch chan int) {
+	Walk(t, ch)
+	close(ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	c1 := make(chan int)
+	c2 := make(chan int)
+	go Walker(t1, c1)
+	go Walker(t2, c2)
+	for i := range c1 {
+		if i != <-c2 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
